e4cb52b58e649dd02249f7d67b34bee525aebd4a
Yaodong Li <ydli@thoughtworks.com>
2017-02-22 21:13:11 +0800

    Complete basic movie item style

--- components/MovieItem.js
+++ components/MovieItem.js
@@ -4,28 +4,89 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
+const propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  originalTitle: PropTypes.string,
+  rating: PropTypes.number,
+  genres: PropTypes.array,
+  cover: PropTypes.string,
+};
+
 class MovieItem extends React.Component {
-  static propTypes = {
-    id: PropTypes.string,
-    title: PropTypes.string,
-    originalTitle: PropTypes.string,
-    rating: PropTypes.number,
-    genres: PropTypes.array,
-    cover: PropTypes.string,
-  }
+  static propTypes = propTypes;
 
   render() {
-    const { title, rating } = this.props;
+    const { id, title, originalTitle, rating, genres, cover } = this.props;
+
     return (
-      <View>
-        <Text>{rating}</Text>
-        <Text>{title}</Text>
+      <View style={styles.container}>
+        <Image
+          style={styles.cover}
+          source={{ uri: cover  }}
+        />
+        <View style={styles.info}>
+          <Text style={styles.rating}>{rating}</Text>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.des}>{originalTitle}</Text>
+          <View style={styles.genres}>
+            {
+              genres.map((x, i) => <Text key={i} style={styles.genresItem}>{x}</Text>)
+            }
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  cover: {
+    width: 65,
+    height: 100
+  },
+  genres: {
+    flex: 0,
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  genresItem: {
+    marginRight: 5,
+    backgroundColor: '#ccc',
+    padding: 3,
+    fontSize: 12,
+    color: '#444',
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  rating: {
+    alignSelf: 'flex-end',
+  },
+  title: {
+    fontSize: 20,
+  },
+  des: {
+    color: '#555',
+  },
+});
 
 export default MovieItem;
--- utils/parser.js
+++ utils/parser.js
@@ -1,7 +1,7 @@
 export const movieItem = source => ({
   id: source.id,
   title: source.title,
-  originalTitle: source.originalTitle,
+  originalTitle: source.original_title,
   rating: source.rating.average,
   genres: source.genres,
   cover: source.images.large,
