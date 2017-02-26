dc24448a01d949fde984a28d31f3f4a27e6dbe2a
Yaodong Li <ydli@thoughtworks.com>
2017-02-26 09:59:48 +0800

    Add navigator component

--- index.ios.js
+++ index.ios.js
@@ -5,54 +5,23 @@ import {
   Text,
   View,
   ScrollView,
+  NavigatorIOS,
 } from 'react-native';
 
-import { getCollection } from './utils/api';
-import * as parser from './utils/parser';
-
-import Item from './components/MovieItem';
+import Home from './components/Home';
 
 export default class movie extends Component {
-  constructor() {
-    super();
-    this.state = {
-      list: [],
-    };
-  }
-  async componentWillMount() {
-    const topMovies = await getCollection('top');
-    this.setState({ list: topMovies.subjects });
-  }
   render() {
-    const items = this.state.list;
     return (
-      <ScrollView style={styles.container}>
-        {
-          items.map((x, i) => <Item {...parser.movieItem(x)} key={i} />)
-        }
-      </ScrollView>
+      <NavigatorIOS
+        style={{ flex: 1 }}
+        initialRoute={{
+          component: Home,
+          title: '豆瓣电影'
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('movie', () => movie);
